0dc3c2af4f71d5e134dd36c3601735df1bf4a1fd
Charles Guo <shaldengeki@gmail.com>
2013-09-11 18:06:30 -0500

    update status

--- views/mal/index.php
+++ views/mal/index.php
@@ -2,6 +2,14 @@
   require_once($_SERVER['DOCUMENT_ROOT']."/app/include.php");
   \Application::check_partial_include(__FILE__);
 ?>
+<h2>SAT stuff is up! <small>09/11/2013</small></h2>
+<p>
+  The SAT stats are largely complete at this point; they're linked in the navbar at the top of the site. I have to get around to making the script that extracts term-topics out of each SAT and post run automatically when an SAT is added to the database, and some of the other stuff (post correlations) isn't up yet, but the general idea is up and running.
+</p>
+<p>
+  I'll work on automating the term-topics next, and after that the MAL scripts and Tweetaboo are up next. Stay tuned for more updates!
+</p>
+<hr />
 <p>
   LLAnim.us is back after upgrading to a shiny new server. I've been hard at work rewriting the site to use the core of the <a href='https://animurecs.com'>Animurecs</a> framework that I've been developing; as a result, <span style="text-decoration: line-through;">everything is dead</span>things are going to take awhile to bring back up.
 </p>
